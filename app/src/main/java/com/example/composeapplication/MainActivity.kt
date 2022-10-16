55249f07ee8b94ee29fb16396fa7071d174ff4cd
Emerson <emersondobl112@gmail.com>
2022-10-16 12:07:26 -0300

    [AULA 2] Alterações visuais no card.

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -6,19 +6,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composeapplication.ui.theme.ComposeApplicationTheme
+import com.example.composeapplication.ui.theme.Purple500
+import com.example.composeapplication.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,19 +35,28 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun ProductItem() {
-    Column {
+    Column(
+        Modifier
+            .width(200.dp)
+            .height(250.dp)
+    ) {
         Box(
             modifier = Modifier
                 .height(100.dp)
                 .fillMaxWidth()
-                .background(Color.Gray)
+                .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
         )
         Image(
             painter = painterResource(id = R.drawable.ic_dialog_alert),
-            contentDescription = "Product image"
+            contentDescription = "Product image",
+            modifier = Modifier
+                .offset(y = (-50).dp)
+                .align(CenterHorizontally)
+                .size(100.dp)
+                .clip(CircleShape)
         )
         Text(text = "Text one")
         Text(text = "Text two")
