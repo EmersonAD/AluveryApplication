55f7ea86f6fb8a210100a810105cc0ec6b4a7ea0
Emerson <emersondobl112@gmail.com>
2022-10-16 13:10:37 -0300

    [AULA 2] Finalização do card de produto.

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -7,7 +7,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +24,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import com.example.composeapplication.ui.theme.ComposeApplicationTheme
 import com.example.composeapplication.ui.theme.Purple500
@@ -42,42 +46,52 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ProductItem() {
-    Column(
-        Modifier
-            .width(200.dp)
-            .height(250.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .height(100.dp)
-                .fillMaxWidth()
-                .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
+    Surface(Modifier.padding(8.dp), shape = RoundedCornerShape(size = 16.dp), elevation = 4.dp) {
+        Column(
+            Modifier
+                .width(200.dp)
+                .heightIn(min = 250.dp, max = 300.dp)
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_dialog_alert),
-                contentDescription = "Product image",
+            val imageSize = 100.dp
+            Box(
                 modifier = Modifier
-                    .offset(y = 50.dp)
-                    .align(BottomCenter)
-                    .size(100.dp)
-                    .clip(CircleShape)
-            )
-        }
-        Spacer(modifier = Modifier.height(50.dp))
-        Column(Modifier.padding(16.dp)) {
-            Text(
-                text = LoremIpsum(50).values.first(),
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 2
-            )
-            Text(
-                text = "R$ 14,99",
-                Modifier.padding(top = 8.dp),
-                fontSize = 14.sp,
-                fontWeight = FontWeight(400)
-            )
+                    .height(imageSize)
+                    .fillMaxWidth()
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500,
+                                Teal200
+                            )
+                        )
+                    )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_dialog_alert),
+                    contentDescription = "Product image",
+                    modifier = Modifier
+                        .offset(y = imageSize/2)
+                        .align(BottomCenter)
+                        .size(imageSize)
+                        .clip(CircleShape)
+                )
+            }
+            Spacer(modifier = Modifier.height(50.dp))
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2
+                )
+                Text(
+                    text = "R$ 14,99",
+                    Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
         }
     }
 }
